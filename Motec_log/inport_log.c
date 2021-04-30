1e5f3451fbeefc4411ca8c9ddba337dea12866d3
Masatsugu <b8123026@gmail.com>
2021-04-30 10:57:32 +0900

    fs

--- Motec_log/inport_log.c
+++ Motec_log/inport_log.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-PROGRAM NAME : airfoil_fixed.c
+PROGRAM NAME : inport_log.c
 AUTHER : Masatsugu Kitadai
 DATE : 18/4/2021
 ******************************************************************************/
@@ -9,9 +9,10 @@ DATE : 18/4/2021
 #include <sys/stat.h>
 #include <math.h>
 #define num 10000
+#define omit 18
 
 double coordinate[num][3];
-char airfoil_name[1000];
+char file_name[1000];
 char buf[128];
 char read_file[1000];
 char output_file[1000];
@@ -22,7 +23,7 @@ FILE *output;
 /*********************************   MAIN   *********************************/
 double main()
 {
-    int i, chord;
+    int i, j, k;
     double x, z;
 
     i = 0;
@@ -30,19 +31,11 @@ double main()
     // 読み込むファイルを指定する
 
     printf("ファイル名：");
-    scanf("%s", airfoil_name);
-
-    /*
-    // 翼弦長を指定する
-
-    printf("翼弦長(mm)：");
-    scanf("%d", &chord);
-*/
+    scanf("%s", file_name);
 
     // 入力・出力ファイル名の指定
 
-    sprintf(read_file, "original//%s.dat", airfoil_name);
-    sprintf(output_file, "result//%s_fixed.dat", airfoil_name);
+    sprintf(read_file, "%s", file_name);
 
     // 読み込むファイルを開く
 
@@ -53,27 +46,24 @@ double main()
         exit(0);
     }
 
-    // 最初の1行を読み飛ばす
+    // 不要な行を読み飛ばす
 
-    fgets(buf, sizeof(buf), read);
+    for (k = 0; k < omit; k++)
+    {
+        fgets(buf, sizeof(buf), read);
+    }
 
-    // 最後の行以外を読み込み，配列に格納する
+    // 最後の行以外を読み込み，配列の大きさを確認
 
     while (fscanf(read, "%lf %lf", &x, &z) != EOF)
     {
         i = i + 1;
-        coordinate[i][1] = x;
-        coordinate[i][2] = 0;
-        coordinate[i][3] = z;
     }
 
     // EOFで判別できない最後の行を読み込む
 
     fscanf(read, "%lf %lf", &x, &z);
     i = i + 1;
-    coordinate[i][1] = x;
-    coordinate[i][2] = 0;
-    coordinate[i][3] = z;
 
     fclose(read);
 
@@ -82,31 +72,23 @@ double main()
     int data_long;
     data_long = i;
 
-    // 指定された翼弦長に修正する
-
-    /*
-    for (i = 1; i < data_long; i++)
-    {
-        coordinate[i][1] = chord * coordinate[i][1];
-        coordinate[i][2] = chord * coordinate[i][2];
-        coordinate[i][3] = chord * coordinate[i][3];
-    }
-*/
+    printf("%d", data_long);
 
     // 書き込むファイルを開く
 
-    output = fopen(output_file, "w");
+    /* output = fopen(output_file, "w");
 
     printf("\nResult-----------------------------------------------------\n\n");
 
     for (i = 1; i < data_long; i++)
     {
         printf("%d\t%lf\t%lf\t%lf\n", i, coordinate[i][1], coordinate[i][2], coordinate[i][3]);
-        fprintf(output, "%lf %lf \n", coordinate[i][1], coordinate[i][3]);
+        fprintf(output, "%d\t%lf\t%lf\t%lf\n", i, coordinate[i][1], coordinate[i][2], coordinate[i][3]);
     }
     printf("\n-----------------------------------------------------------\n");
 
     fclose(output);
+*/
 
     return (0);
 }
