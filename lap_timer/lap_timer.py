0413b0debec27981f87181e3f270f68da6df5668
Masatsugu <b8123026@gmail.com>
2021-05-30 23:06:14 +0900

    anaconda

---
+++ lap_timer/lap_timer.py
@@ -0,0 +1,50 @@
+import time
+import cv2
+import numpy as np
+
+start = time.time()
+
+# webcam for ubuntu
+movie = cv2.VideoCapture(2)
+
+
+before = None
+fps = int(movie.get(cv2.CAP_PROP_FPS))
+ret, frame = movie.read()
+
+while True:
+
+    ret, frame = movie.read()
+    if ret == False:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if before is None:
+        before = gray.astype("float")
+        continue
+
+    cv2.accumulateWeighted(gray, before, 0.95)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(before))
+
+    thresh = cv2.threshold(frameDelta, 3, 255, cv2.THRESH_BINARY)[1]
+
+    contours = cv2.findContours(
+        thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    # pixel_number = np.size(contours)
+    pixel_sum = np.sum(contours)
+    white_pixel_number = pixel_sum/255
+    print(white_pixel_number)
+
+    if white_pixel_number > 9000:
+        lap_start = time.time()
+        total_time = time.time() - start
+        lap_time = time.time() - lap_start
+        print(total_time)
+        print(lap_time)
+
+    elif cv2.waitKey(1) == 13:
+        break
+
+cv2.destroyAllWindows()  # ウィンドウを破棄
