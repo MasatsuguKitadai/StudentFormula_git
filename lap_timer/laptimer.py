14a81d75f21bf9bd8b39cd0d3a961b76145307d0
MasatsuguKitadai <b8123026@gmail.com>
2021-05-31 13:46:07 +0900

    lab

--- lap_timer/laptimer.py
+++ lap_timer/laptimer.py
@@ -5,10 +5,14 @@ import numpy as np
 start = time.time()
 
 # webcam for ubuntu
-movie = cv2.VideoCapture(2)
+# movie = cv2.VideoCapture(2)
+
+# webcamera for windows
+movie = cv2.VideoCapture(1)
 
 before = None
 fps = int(movie.get(cv2.CAP_PROP_FPS))
+time.sleep(1/fps)
 ret, frame = movie.read()
 
 lap_start = 0
@@ -33,20 +37,20 @@ while True:
     contours = cv2.findContours(
         thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-    # cv2.imshow("frame", contours)
+    cv2.imshow("frame", contours)
 
     pixel_sum = np.sum(contours)
     white_pixel_number = pixel_sum/255
     # print(white_pixel_number)
 
-    if white_pixel_number > 10000:
+    if white_pixel_number > 9000:
         total_time = time.time() - start
         lap_time = time.time() - lap_start
         lap_start = time.time()
         print(total_time)
         print(lap_time)
 
-    # elif cv2.waitKey(0) == 13:
-    #    break
+    elif cv2.waitKey(0) == 13:
+        break
 
 cv2.destroyAllWindows()  # ウィンドウを破棄
